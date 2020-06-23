36a8296f0a6898474ec37729c3f367c771b3685c
doblefilmina <brusafederico@gmail.com>
2020-06-23 19:25:04 -0300

    envio de mails con nodemailer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const multer = require('multer')
 const joi = require('@hapi/joi')
+const nodemailer = require('nodemailer')
 const server = express()
 
 const public = express.static('public')
@@ -8,30 +9,38 @@ const urlencoded = express.urlencoded({extended : true}) // con esta configuraci
 const json = express.json()
 const upload = multer()
 
-const schemaContact = joi.object( {
-    nombre : joi.string().alphanum().min(4).max(25).required(),
+const schemaContact = joi.object({
+    nombre : joi.string().min(4).max(25).required(),
     correo: joi.string().email({ 
         minDomainSegments: 2,
         tlds: {
              allow: ['com', 'net', 'org']
      }
-    }),
+    }).required(),
     asunto : joi.string().alphanum().valid('ax14', 'ax38', 'ax45', 'ax67').required(),
-    mensaje: joi.string().alphanum().min(10).max(100).required()
+    mensaje: joi.string().min(5).max(100).required()
+})
+
+// 1) crear la conexión con el servidor de email
+const miniOutlook = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'ariel.williamson35@ethereal.email',
+        pass: 'mhWyEjsneAzk7bKVBb'
+    }
+});
+
+// 2) verificar la conexión con el servidor de email
+miniOutlook.verify( (error, ok) => {
+    error ? console.log('AHHHH!!!') : console.log('Todo legal')
 })
 
         // Middleware
 server.use( json )
-server.use ( urlencoded )
 server.use( upload.array() )
 server.use( public )
-
-const datos = {
-    nombre: "Luis Miguel",
-    correo: "luis@miguel.com",
-    asunto: "ax14",
-    mensaje: "No culpers a la noche.."
-}
+server.use( urlencoded )
 
 
 server.post('/enviar', function (request, response) {
@@ -42,7 +51,23 @@ server.post('/enviar', function (request, response) {
   console.log(datos)
 
   const validacion = schemaContact.validate( datos )
-  response.json({ rta: 'ok'})
+
+  if( validacion.error) { 
+  response.json({ rta: 'error', details : validacion.error.details })
+
+  } else { //si los datos son validos.. hacer magia
+    miniOutlook.sendMail({
+        from : datos.correo,
+        to : "fede_brusa@hotmail.com",
+        subject : "consulta desde la oscuridad del servidor",
+        html : '<h1>Hola viteh!</h1>'
+    }, function(error, info){
+        const rta = error ? "su consulta no pudo ser enviada" : "gracias por su consulta"
+        response.json({rta})
+    })
+    
+   
+  }
   
 })
  
