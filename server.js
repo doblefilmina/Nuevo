36e761822f7900879cb4e649182be5c9e8a5d285
doblefilmina <brusafederico@gmail.com>
2020-06-30 21:14:15 -0300

    buscador de productos por id

--- server.js
+++ server.js
@@ -1,38 +1,81 @@
 const express = require('express')
-
+const { MongoClient } = require('mongodb')
+const { ObjectId } = require('mongodb')
 const server = express()
 
-const urlencoded = express.urlencoded({extended : true}) // con esta configuración, todo lo que viene en url encoded, se interpreta como objeto
+
+const urlencoded = express.urlencoded({ extended : true })
 const json = express.json()
 
-const DB = []
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/${process.env.MONGODB_BASE}?retryWrites=true&w=majority`
+
+const connectDB = async () =>{
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true } )
+    
+    DB = await client.db("MercadoTECH")
+}
+
+
+let DB = null
+
+connectDB()
+
+console.log('El servidor de MongoDB es: ')
+console.log(process.env.MONGODB_HOST)
 
 server.use( json )
 server.use( urlencoded )
 server.listen(3000)
 
-server.get('/api', (req, res) => {      //api para obtener los datos
-    res.json(DB)
+server.get('/api', async (req, res) => {      //api para obtener los datos
+    
+    const productos = await DB.collection('Productos')
+    const resultado = await productos.find({}).toArray() 
+    res.json( resultado )
 }) 
-server.post('/api', (req, res) => {     // api para crer con datos
-    const datos = req.body
 
+server.get('/api/:id' , async (req, res) =>{
+
+    const productos = await DB.collection('Productos')
+
+    const ID = req.params.id
+    const query = { '_id' : ObjectId(ID) }
+    const resultado = await productos.find( query ).toArray() 
+
+    res.json( resultado )
+
+
+})
+
+
+
+server.post('/api', async (req, res) => {     // api para crer con datos
+    const datos = req.body
+    const productos = await DB.collection('Productos')
+    
+
+    
+    const resultado = await productos.insertOne( datos )
+     
 /*
 REQUISITOS DEL ID:
     - Unico
     - Irrepetible
     - autoasignable
 */
-
+/*
 
 const id = new Date().getTime()
 
     DB.push({id, ...datos }) //con la estructura "...datos" se rompe el objeto, y cada propiedad del objeto se transforma en una variable nueva. {...datos} lo rompe y lo vuelve a armar. esto se hace para agregarle una propiedad más al objeto.
 
     console.log(DB)
-
-    res.json({ rta : 'ok'})
+*/
+    const { result } = await productos.insertOne( datos )
+    res.json({ rta : result.ok })
 }) 
+
+
 server.put('/api', (req, res) => {          // api para actualizar con datos
     const datos = req.body
 
