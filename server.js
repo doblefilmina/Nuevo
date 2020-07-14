99fef3f9d216128c87c939a92b1e9eb8be4375de
doblefilmina <brusafederico@gmail.com>
2020-07-14 19:50:43 -0300

    rutas preparadas para editar y agregar producto

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('express-handlebars')
 const { MongoClient } = require('mongodb')
 const { ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
 const server = express()
 
 
@@ -14,16 +15,10 @@ const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PAS
 const connectDB = async () =>{
     const client = await MongoClient.connect(url, { useUnifiedTopology: true } )
     
-    DB = await client.db("MercadoTECH")
+    return await client.db("MercadoTECH")
 }
 
-
-let DB = null
-
-connectDB()
-
-console.log('El servidor de MongoDB es: ')
-console.log(process.env.MONGODB_HOST)
+const port = process.env.PORT || 3000
 
 server.use( json )
 server.use( urlencoded )
@@ -31,36 +26,44 @@ server.set('view engine', 'handlebars')
 server.engine( 'handlebars', hbs() )
 
 server.use('/', public)
-server.listen(3000)
+server.listen(port)
 
 
 // Inicio de Rutas del Dashboard //
-server.get('/admin', (req, res) =>{
+server.get('/admin', async (req, res) =>{
+    const DB = await connectDB()
+    const productos = await DB.collection('Productos')
+    const resultado = await productos.find({}).toArray()
 
-    const productos = [{"_id":"5ef541d4ca97fcfffe753769","nombre":"5ef541d4ca97fcfffe753769","stock":"5","precio":"6900","disponible":"true","marca":"tu vieja"},{"_id":"5efbc65f6655a41144e74c03","nombre":"Jugo de Limon","stock":"999","precio":"99.99","marca":"Lemoncito"},{"_id":"5efbc7bc6655a41144e74c04","nombre":"Pan de queso","stock":"500","precio":"300","marca":"Quesolito"},{"_id":"5efe547bbceae77e54fcbad9","nombre":"leche","stock":"780","precio":"54","marca":"devaca","disponibilidad":"true"},{"_id":"5efe5d30814ab28cc096079d","nombre":"leche","stock":"780","precio":"54","marca":"devaca","disponibilidad":"true"},{"_id":"5efe5e6732662f5fe4d846f5","nombre":"caramelos","stock":"43","precio":"8","marca":"flin-paf","disponibilidad":"true","disponible":"true"},{"_id":"5efe5f19443dcd8c7475bf5e","nombre":"Lavarropas","stock":"5","precio":"6900","marca":"devaca","disponibilidad":"true","Marca":"florencia","disponible":"true"},{"_id":"5efe6b808d75c482303b8fc1","nombre":"iPhone X","stock":"500","precio":"699","asunto":"apple","detalle":"Modelo A6253 v4 64GB - LTE - Wifi 802.11n"}]
-        
-    res.render('listado', {productos})
+    console.log('los productos son:')
+    console.log( resultado )
+    res.render('main', { 
+        layout : false,
+        items : resultado,
+        url : req.protocol + "://"+ req.hostname + ":"+ port // <-- http://localhost:3000
+    })
 
-} )
+})
 
-server.get('/admin/contacto', (req, res) =>{
-    
-    res.render('listado', {ACCION: 'productos'})
+server.get('/admin/editar/:id', async (req, res) =>{
+    res.end(`Aca hay que editar el producto: ${req.params.id}`)
+})
 
-} )
-
+server.get('/admin/nuevo', async (req, res) =>{
+    res.end(`Aca vamos a ingresar un nuevo producto`)
+})
 
 // Fin de rutas del Dashboard //
 
 server.get('/api', async (req, res) => {      //api para obtener los datos
-    
+    const DB = await connectDB()
     const productos = await DB.collection('Productos')
     const resultado = await productos.find({}).toArray() 
     res.json( resultado )
 }) 
 
 server.get('/api/:id' , async (req, res) =>{
-
+    const DB = await connectDB()
     const productos = await DB.collection('Productos')
 
     const ID = req.params.id
@@ -130,4 +133,27 @@ server.delete('/api/:id', async (req, res) => {       //api para eliminar los da
     const result = await productos.findOneAndDelete( query )
     
     res.json({ rta : 'result.ok'})
+
 }) 
+
+/////////////JWT Test ///////////////
+server.post('/login', (req, res) => {
+
+    const datos = req.body
+
+    if( datos.email == 'pepito@gmail.com' && datos.clave == 'HolaDonPepito2020') {
+        
+        const token = jwt.sign({})
+        
+        res.json({ rta : 'Estas logeado' })
+
+    } else  {
+
+        res.json({ rta: 'datos incorrectos'})
+    }
+
+})
+
+server.get('/check', (req,res) => {
+
+})
