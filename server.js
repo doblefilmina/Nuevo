a9a9858832aea13160f940f2b8c8f45a41bf66cc
doblefilmina <brusafederico@gmail.com>
2020-07-14 22:07:16 -0300

    Incorporando cookies para JWT login

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const hbs = require('express-handlebars')
 const { MongoClient } = require('mongodb')
 const { ObjectId } = require('mongodb')
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const server = express()
 
 
 const urlencoded = express.urlencoded({ extended : true })
 const json = express.json()
 const public = express.static(__dirname + '/public')
+const cookies = cookieParser()
 
 const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/${process.env.MONGODB_BASE}?retryWrites=true&w=majority`
 
@@ -22,21 +24,37 @@ const port = process.env.PORT || 3000
 
 server.use( json )
 server.use( urlencoded )
+server.use( cookies)
 server.set('view engine', 'handlebars')
 server.engine( 'handlebars', hbs() )
 
 server.use('/', public)
 server.listen(port)
 
+/////////////JWT Test ///////////////
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies._auth   //verifica el token
+    console.log(token)
+
+    jwt.verify(token, process.env.JWT_PASSPHRASE, (error,data) => {
+        if (error) {
+            res.redirect('http://localhost:3000/admin/ingresar')
+        } else {
+            req.user = data.usuario
+            next()
+        }
+    })
+}
+
 
 // Inicio de Rutas del Dashboard //
 server.get('/admin', async (req, res) =>{
     const DB = await connectDB()
+
     const productos = await DB.collection('Productos')
     const resultado = await productos.find({}).toArray()
 
-    console.log('los productos son:')
-    console.log( resultado )
     res.render('main', { 
         layout : false,
         items : resultado,
@@ -45,13 +63,18 @@ server.get('/admin', async (req, res) =>{
 
 })
 
+server.get('/admin/nuevo', verifyToken, async (req, res) =>{
+    const hansel = req.cookies._auth
+    res.write(`<p> El token de la cookie es: ${hansel}</p>`)
+    res.end(`Aca hay que agregar nuevo producto`)
+   
+})
+
 server.get('/admin/editar/:id', async (req, res) =>{
     res.end(`Aca hay que editar el producto: ${req.params.id}`)
 })
 
-server.get('/admin/nuevo', async (req, res) =>{
-    res.end(`Aca vamos a ingresar un nuevo producto`)
-})
+
 
 server.get('/admin/ingresar', async (req, res) =>{
     res.render('login', {layout: false})
@@ -59,6 +82,8 @@ server.get('/admin/ingresar', async (req, res) =>{
 
 // Fin de rutas del Dashboard //
 
+
+//////// API REST //////////////
 server.get('/api', async (req, res) => {      //api para obtener los datos
     const DB = await connectDB()
     const productos = await DB.collection('Productos')
@@ -120,28 +145,9 @@ server.put('/api/:id', async (req, res) => {          // api para actualizar con
 
     const resultado = await productos.updateOne( query, update )
 
-
-    const verifyToken = (req, res, next) => {
-        const token = req.query.token   //verifica el token
-        console.log(token)
-
-        jwt.verify(token, process.env.JWT_PASSPHRASE, (error,data) => {
-            if (error) {
-                res.json({rta: "Acceso no autorizado"})
-            } else {
-                req.user = data.usuario
-                next()
-            }
-        })
-    }
-
-   
-
-  /*  const encontrado = DB.find(item => item.id == datos.id)
-    encontrado.stock = datos.stock
-*/
     res.json({ rta : 'ok'})
 }) 
+
 server.delete('/api/:id', async (req, res) => {       //api para eliminar los datos
     const ID = req.params.id
 
@@ -153,26 +159,36 @@ server.delete('/api/:id', async (req, res) => {       //api para eliminar los da
     
     res.json({ rta : 'result.ok'})
 
-}) 
+})
 
-/////////////JWT Test ///////////////
+       
+
+  /*  const encontrado = DB.find(item => item.id == datos.id)
+    encontrado.stock = datos.stock
+*/
+    
+ 
+
+//////// JWT login //////////////
+
+
 server.post('/login', (req, res) => {
 
     const datos = req.body
 
     if( datos.email == 'pepito@gmail.com' && datos.clave == 'HolaDonPepito2020') {
+
+        const vencimientoTimestamp = Date.now() + 60*1000*5     //dentro de 5 minutos (en milisegundos)
+        const vencimientoFecha = new Date( vencimientoTimestamp ) 
         
         const token = jwt.sign({ usuario : datos.email, expiresIn : 60}, process.env.JWt_PASSPHRASE)
         
-        res.json({ rta : 'Estas logeado', token })
+        res.cookie("_auth", token, { expires: vencimientoFecha, httpOnly: true , sameSite: "Strict", secure: false })
 
+        res.redirect('http://localhost:3000/admin')
     } else  {
 
         res.json({ rta: 'datos incorrectos'})
     }
 
-})
-
-server.get('/check', (req,res) => {
-    res.end(`Bienvenida/o $`)
 })
